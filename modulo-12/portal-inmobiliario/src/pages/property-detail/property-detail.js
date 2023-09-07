2a39bc35bc8c957197e50f15e6fe8048e759ff7b
Fran Lopez <franciscolopto@gmail.com>
2023-09-07 13:15:52 +0200

    update use promise all

--- modulo-12/portal-inmobiliario/src/pages/property-detail/property-detail.js
+++ modulo-12/portal-inmobiliario/src/pages/property-detail/property-detail.js
@@ -1,4 +1,8 @@
-import { getPropertyList, postContact } from './property-detail.api';
+import {
+  getPropertyList,
+  postContact,
+  getEquipmentsList,
+} from './property-detail.api';
 import { history } from '../../core/router';
 import { mapPropertyDetailFromApiToVM } from './property-detail.mappers';
 import { setPropertyValues } from './property-detail.helpers';
@@ -12,47 +16,60 @@ import { contactFormValidation } from './property-detail.validations';
 
 const params = history.getParams();
 
-getPropertyList(params.id).then((propertyList) => {
-  const viewModelPropertyList = mapPropertyDetailFromApiToVM(propertyList);
-  setPropertyValues(viewModelPropertyList);
-});
+Promise.all([getPropertyList(params.id), getEquipmentsList()]).then(
+  ([propertyList, equipmentsList]) => {
+    const viewModelPropertyList = mapPropertyDetailFromApiToVM(
+      propertyList,
+      equipmentsList
+    );
+    setPropertyValues(viewModelPropertyList);
+  }
+);
 
-let contact = {
+// aqui solo hace una promesa, faltaria hacer lo mismo para el equipmentsList y arriba las hace todas a la vez
+//   getPropertyList(params.id).then((propertyList) => {
+//   const viewModelPropertyList = mapPropertyDetailFromApiToVM(propertyList);
+//   setPropertyValues(viewModelPropertyList);
+// });
+
+let formData = {
   email: '',
   message: '',
 };
 
 onUpdateField('email', (event) => {
   const value = event.target.value;
-  contact = {
-    ...contact,
+  formData = {
+    ...formData,
     email: value,
   };
 
-  contactFormValidation.validateField('email', contact.email).then((result) => {
-    onSetError('email', result);
-  });
+  contactFormValidation
+    .validateField('email', formData.email)
+    .then((result) => {
+      onSetError('email', result);
+    });
 });
 
 onUpdateField('message', (event) => {
   const value = event.target.value;
-  contact = {
-    ...contact,
+  formData = {
+    ...formData,
     message: value,
   };
 
   contactFormValidation
-    .validateField('message', contact.message)
+    .validateField('message', formData.message)
     .then((result) => {
       onSetError('message', result);
     });
 });
 
 onSubmitForm('contact-button', () => {
-  contactFormValidation.validateForm(contact).then((result) => {
+  contactFormValidation.validateForm(formData).then((result) => {
     onSetFormErrors(result);
     if (result.succeeded) {
-      postContact(contact);
+      postContact(formData);
     } else {
       alert('Por favor, debe completar todos los campos requeridos.');
     }
